3434b272198a6fc98c6bd1a0ff4008906706660f
Marcos Paulo <marcos.nascimento19@gmail.com>
2022-08-25 05:13:36 -0400

    criando objetos

---
+++ Dio/semana-frontend-mundo-invertido/assets/js/main.js
@@ -0,0 +1,7 @@
+const txtName = document.getElementById('txtNAme')
+const txtEmail = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
+const btnSubscribe = document.getElementById('btnSubscribe')
+debugger
+console.log('txtNAme!')
